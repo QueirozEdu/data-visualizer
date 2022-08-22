7ce0cdd4677014ec2cf0fea69517e687fed1c2ed
Eduardo <eduhmq@gmail.com>
2022-08-22 12:32:43 -0300

    js function to change elements value to corresponding cata

--- services/index.js
+++ services/index.js
@@ -1,7 +1,25 @@
 async function readJson() {
     const data = await fetch('./services/data.json');
-     let dataInfo = await data.json();
-     console.log(dataInfo["storage"]);
+    let dataInfo = await data.json();
+    
+    
+    document.getElementById("bar__final-value").innerText = dataInfo["storage"].concat("GB");
+    
+    let fullStorage = dataInfo["storage"];
+    let usedStorage = dataInfo["used"];
+    let barWidth = (usedStorage / fullStorage) * 100;
+
+    let barPercentage = barWidth.toString();
+    let finalBarWidth = barPercentage.concat("%");
+    document.getElementById("chart__bar").style.width = finalBarWidth;
+
+
+    document.getElementById("data__visualization-header--title").innerText = "You've used ".concat(usedStorage).concat(" GB of your storage")
+    document.getElementById("space__left-value").innerHTML = fullStorage - usedStorage;
+
 }
 
-// setInterval(readJson(), 1000);
+
+
+
+readJson();
