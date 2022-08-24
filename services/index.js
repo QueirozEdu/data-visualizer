cdbea6acf1fcedc00cf2814b0b0ce8868650aec3
Eduardo <eduhmq@gmail.com>
2022-08-24 13:04:01 -0300

    js to show mock data

--- services/index.js
+++ services/index.js
@@ -1,13 +1,32 @@
-async function readJson() {
-    const data = await fetch('./services/data.json');
-    let dataInfo = await data.json();
+async function showSliderData() {
+    // const data = await fetch('./services/data.json');
+    // let dataInfo = await data.json();
+
+    let storageAvailable = document.getElementById("storage__available-slider").value;
+    let storageUsed = document.getElementById("storage__used-slider").value;
+
+    if (storageUsed > storageAvailable) {
+        document.getElementById("storage__used-slider").value = storageAvailable;
+        document.getElementById("space__left-value").innerText = "0";
+        document.getElementById("data__visualization-header--title").innerText = "You've used ".concat(usedStorage).concat(" GB of your storage") 
+
+        document.getElementById("chart__bar").style.width = "99%";
+    }
+    else {
+
+
+        
+    // document.getElementById("bar__final-value").innerText = dataInfo["storage"].concat("GB");
+
+    document.getElementById("bar__final-value").innerText = storageAvailable;
     
-    
-    document.getElementById("bar__final-value").innerText = dataInfo["storage"].concat("GB");
-    
-    let fullStorage = dataInfo["storage"];
-    let usedStorage = dataInfo["used"];
-    let barWidth = (usedStorage / fullStorage) * 100;
+    // let fullStorage = dataInfo["storage"];
+    // let usedStorage = dataInfo["used"];
+    // let barWidth = (usedStorage / fullStorage) * 100;
+
+    let fullStorage = storageAvailable;
+    let usedStorage = storageUsed;
+    let barWidth = (usedStorage / storageAvailable) * 100;
 
     let barPercentage = barWidth.toString();
     let finalBarWidth = barPercentage.concat("%");
@@ -16,10 +35,10 @@ async function readJson() {
 
     document.getElementById("data__visualization-header--title").innerText = "You've used ".concat(usedStorage).concat(" GB of your storage") 
     document.getElementById("space__left-value").innerHTML = fullStorage - usedStorage;
+    }
 
 }
 
 
 
-
-readJson();
+// showSliderData();
